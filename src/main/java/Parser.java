256b2bf1246c1609d7ca36f301b7ec30e24abc98
HosseinAnbaz <hossien.anbaz@yahoo.com>
2025-04-23 22:01:25 +0330

    change files

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -9,40 +9,61 @@ import java.util.*;
 public class Parser {
     static List<Game> games = new ArrayList<>();
 
-    public List<Game> sortByName(){
+    public List<Game> sortByName() {
         List<Game> sortedByName = new ArrayList<>(games);
-        // Sort games alphabetically (least)
-        //TODO
-        return  sortedByName;
+        sortedByName.sort(Comparator.comparing(Game::getName));
+        return sortedByName;
     }
 
-    public List<Game> sortByRating(){
+    public List<Game> sortByRating() {
         List<Game> sortedByRating = new ArrayList<>(games);
-        // Sort games by rating (most)
-        //TODO
+        sortedByRating.sort(Comparator.comparingDouble(Game::getRating).reversed());
         return sortedByRating;
     }
 
-    public List<Game> sortByPrice(){
+    public List<Game> sortByPrice() {
         List<Game> sortedByPrice = new ArrayList<>(games);
-        // Sort games by price (most)
-        //TODO
+        sortedByPrice.sort(Comparator.comparingInt(Game::getPrice).reversed());
         return sortedByPrice;
     }
 
     public void setUp() throws IOException {
+        File input = new File("src/Resources/games.html");
+        Document doc = Jsoup.parse(input, "UTF-8");
 
-        //Parse the HTML file using Jsoup
-        //TODO
+        Elements gameElements = doc.select("div.game");
 
-        // Extract data from the HTML
-        //TODO
+        for (Element gameElement : gameElements) {
+            String name = gameElement.select("span.game-name").text();
+            String ratingText = gameElement.select("span.game-rating").text();
+            String priceText = gameElement.select("span.game-price").text();
 
-        // Iterate through each Game div to extract Game data
-        //TODO
+            try {
+                double rating = Double.parseDouble(ratingText);
+                int price = Integer.parseInt(priceText);
+                games.add(new Game(name, rating, price));
+            } catch (NumberFormatException e) {
+                System.out.println("Skipping invalid entry: " + name);
+            }
+        }
     }
 
     public static void main(String[] args) {
-        //you can test your code here before you run the unit tests
+        Parser parser = new Parser();
+        try {
+            parser.setUp();
+
+            System.out.println("Sorted by Name:");
+            parser.sortByName().forEach(System.out::println);
+
+            System.out.println("\nSorted by Rating:");
+            parser.sortByRating().forEach(System.out::println);
+
+            System.out.println("\nSorted by Price:");
+            parser.sortByPrice().forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
